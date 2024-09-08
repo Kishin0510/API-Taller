eb3939ee61b50a40503d966e8e10c32e325ae342
Kishin520 <nicokishins@hotmail.com>
2024-09-07 20:55:39 -0400

    feat: Modelos agregados al proyecto

---
+++ src/Models/User.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_Taller.src.Models
+{
+    public class User
+    {
+        
+    }
+}

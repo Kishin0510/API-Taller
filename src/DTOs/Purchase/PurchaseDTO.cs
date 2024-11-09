cd276115b4c249b57247e1c402fed36e05052cc5
IgnacioCarvajal070 <130288880+IgnacioCarvajal070@users.noreply.github.com>
2024-11-09 17:00:09 -0300

    feat: AddProduct y Product DTOs agregados

---
+++ src/DTOs/Purchase/PurchaseDTO.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api_Taller.src.DTOs.Product;
+using Bogus.DataSets;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api_Taller.src.DTOs.Purchase
+{
+    public class PurchaseDTO
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public List<ProductDTO> Products { get; set; } = new List<ProductDTO>();
+        public List<int> Quantities { get; set; } = new List<int>();
+        public int TotalPrice { get; set; }
+        public string Country { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string Comune { get; set; } = null!;
+        public string Street { get; set; } = null!;
+    }
+}
